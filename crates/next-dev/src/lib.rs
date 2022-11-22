vercel/turborepo

Support basic serving of next/image images
tmp

crates/next-dev/src/lib.rs
@@ -8,7 +8,7 @@ use std::{collections::HashSet, env::current_dir, net::IpAddr, path::MAIN_SEPARA
 use anyhow::{anyhow, Context, Result};
 use next_core::{
     create_app_source, create_server_rendered_source, create_web_entry_source, env::load_env,
-    source_map::NextSourceMapTraceContentSourceVc,
+    next_image::NextImageContentSourceVc, source_map::NextSourceMapTraceContentSourceVc,
 };
 use turbo_tasks::{
     primitives::StringsVc, RawVc, TransientInstance, TransientValue, TurboTasks, Value,
@@ -131,7 +131,7 @@ impl NextDevServerBuilder {
         let console_ui = Arc::new(ConsoleUi::new(log_options));
         let console_ui_to_dev_server = console_ui.clone();
 
-        let server = DevServer::listen(
+        DevServer::listen(
             turbo_tasks.clone(),
             move || {
                 source(
@@ -151,9 +151,7 @@ impl NextDevServerBuilder {
             )
                 .into(),
             console_ui_to_dev_server,
-        );
-
-        server
+        )
     }
 }
 
@@ -255,6 +253,7 @@ async fn source(
     .cell()
     .into();
     let source_map_trace = NextSourceMapTraceContentSourceVc::new(rendered_source).into();
+    let img_source = NextImageContentSourceVc::new(rendered_source).into();
     let source = RouterContentSource {
         routes: vec![
             ("__turbopack__/".to_string(), introspect),
@@ -263,6 +262,8 @@ async fn source(
                 "__nextjs_original-stack-frame".to_string(),
                 source_map_trace,
             ),
+            // TODO: Load path from next.config.js
+            ("_next/image".to_string(), img_source),
         ],
         fallback: main_source.into(),
     }

nagaozen/turbo-1

crates/next-dev/src/lib.rs
@@ -18,7 +18,7 @@ use anyhow::{anyhow, Context, Result};
 use devserver_options::DevServerOptions;
 use next_core::{
     create_app_source, create_server_rendered_source, create_web_entry_source, env::load_env,
-    source_map::NextSourceMapTraceContentSourceVc,
+    next_image::NextImageContentSourceVc, source_map::NextSourceMapTraceContentSourceVc,
 };
 use owo_colors::OwoColorize;
 use turbo_tasks::{
@@ -322,7 +322,9 @@ async fn source(
     }
     .cell()
     .into();
-    let source_map_trace = NextSourceMapTraceContentSourceVc::new(main_source.into()).into();
+    let main_source = main_source.into();
+    let source_map_trace = NextSourceMapTraceContentSourceVc::new(main_source).into();
+    let img_source = NextImageContentSourceVc::new(main_source).into();
     let source = RouterContentSource {
         routes: vec![
             ("__turbopack__/".to_string(), introspect),
@@ -331,8 +333,10 @@ async fn source(
                 "__nextjs_original-stack-frame".to_string(),
                 source_map_trace,
             ),
+            // TODO: Load path from next.config.js
+            ("_next/image".to_string(), img_source),
         ],
-        fallback: main_source.into(),
+        fallback: main_source,
     }
     .cell()
     .into();